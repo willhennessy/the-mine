ce5a18714cc714a1cde4fc3f863aa8a6e5f6e007
Will Hennessy <hennessywill@gmail.com>
2021-09-04 18:25:54 -0500

    init Mine contract, add typescript, convert test and deploy script (#1)

---
+++ scripts/deploy.ts
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { Contract, ContractFactory } from "ethers";
+import { ethers } from "hardhat";
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Mine: ContractFactory = await ethers.getContractFactory("Mine");
+  const mine: Contract = await Mine.deploy(/*constructor args*/);
+  await mine.deployed();
+
+  console.log("Mine deployed to: ", mine.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+});
